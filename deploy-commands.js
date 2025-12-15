152d827fd684f9d8d75a60aa79f1e2f7050283d6
contanitro02873837-lgtm <contanitro02873837@gmail.com>
2025-12-15 04:38:24 -0300

    Create deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+const { REST, Routes } = require('discord.js');
+require('dotenv').config();
+const fs = require('fs');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Registrando comandos...');
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands },
+        );
+        console.log('Comandos registrados com sucesso!');
+    } catch (error) {
+        console.error(error);
+    }
+})();
